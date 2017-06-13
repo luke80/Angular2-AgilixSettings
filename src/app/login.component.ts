021fefe09a9f3d55da24dbac8ae67603648061ab
Luke Rebarchik <myfavoriteluke@gmail.com>
2017-06-12 22:46:58 -0600

    update login to include select for domain

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -6,13 +6,27 @@ import { AuthService } from './services/auth.service'
 @Component({
   templateUrl: 'login.component.html',
   styles: [`
+    .form-group {
+      position: relative;
+      margin: 1em auto;
+    }
     em {
-      float:right; color:#E05C65; padding-left: 10px;
+      position:absolute;top:0;right:0;
+      color:#E05C65;
+      padding-left: 10px;
     } 
   `]
 })
 
 export class LoginComponent {
+  public userspace: string = 'byuis';
+  public userspaces = [
+    {domainId: 1, domainTitle: 'Main Domain', domain: 'domain'},
+    {domainId: 2, domainTitle: 'Other Domain', domain: 'something'},
+    {domainId: 3, domainTitle: 'Domainish', domain: 'ish'},
+    {domainId: 4, domainTitle: 'Secondary Domain', domain: 'secondary'}
+  ];
+
   constructor(private authService: AuthService, private router: Router) {
 
   }
