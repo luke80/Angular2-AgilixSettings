cc44fef0b605e9b8a05009a9826501a2b4b6710d
Luke Rebarchik <myfavoriteluke@gmail.com>
2017-07-28 22:25:45 -0500

    update routes to reflect recent changes (component additions)

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,14 +4,18 @@ import { WelcomeComponent } from './welcome.component'
 import { DomainListComponent } from './domain-list.component'
 import { LoginComponent } from './login.component'
 import { MaterialDemoComponent } from './material-demo/material-demo.component'
+import { CourseListComponent } from './course-list.component'
+import { CourseSearchComponent } from './course-search.component'
 
 import { AuthRouteActivator } from './services/auth-route-activator.service'
 
 export const appRoutes:Routes = [
-  { path: 'welcome', component: WelcomeComponent, canActivate: [ AuthRouteActivator ] },
-  { path: 'list-domains', component: DomainListComponent, canActivate: [ AuthRouteActivator ] },
-  { path: 'demo', component: MaterialDemoComponent },
+  { path: 'domain/:domainid/search-courses/:operation', component: CourseSearchComponent, pathMatch: 'full' },
+  { path: 'domain/:domainid', component: CourseListComponent, pathMatch: 'full' },
+  { path: 'list-domains', component: DomainListComponent, canActivate: [ AuthRouteActivator ], pathMatch: 'full' },
+  { path: 'demo', component: MaterialDemoComponent, pathMatch: 'full' },
   { path: 'login?token=:token', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeComponent, canActivate: [ AuthRouteActivator ], pathMatch: 'full' },
   { path: '', redirectTo: '/welcome', pathMatch: 'full'},
 ];
